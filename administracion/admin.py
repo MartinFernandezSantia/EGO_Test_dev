80862870932609abce3218cbc3e76450d50a66e3
Martin Fernandez Santia <martin-santia@hotmail.com.ar>
2023-12-16 20:35:26 -0300

    Finalizado panel admin

--- administracion/admin.py
+++ administracion/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from modelos.models import Vehiculos, ModelosVehiculos, Imagenes, Colores
 
-# Register your models here.
+
+class InlineModelosVehiculos(admin.TabularInline):
+    model = ModelosVehiculos
+    extra = 0
+
+class InlineImagenes(admin.TabularInline):
+    model = Imagenes
+    extra = 1
+
+class AdminVehiculos(admin.ModelAdmin):
+    inlines = [InlineModelosVehiculos, InlineImagenes]
+
+    list_display = ["vehiculo", "price_formatted", "fecha", "tipo"]
+    list_filter = ("tipo", "fecha")
+
+    search_fields = ("vehiculo",)
+
+class AdminModelosVehiculos(admin.ModelAdmin):
+    list_display = ["modelo", "vehiculo"]
+
+admin.site.register(Vehiculos, AdminVehiculos)
+admin.site.register(ModelosVehiculos, AdminModelosVehiculos)
+admin.site.register(Imagenes)
+admin.site.register(Colores)
+
--- modelos/models.py
+++ modelos/models.py
@@ -1,16 +1,31 @@
 from django.db import models
-from django.utils import timezone
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+import os
+from django.core.exceptions import ValidationError
+from django.contrib import admin
 
-# Create your models here.
+
+# Colores de los vehiculos
 class Colores(models.Model):
+    class Meta:  
+        verbose_name_plural = 'Colores'
+
     id_colores = models.AutoField(primary_key=True)
     color = models.CharField(max_length=30, null=False, blank=False)
 
-class Imagenes(models.Model):
-    id_imagen_modelo = models.AutoField(primary_key=True)
-    imagen = models.ImageField(null=False, blank=False, upload_to="imagenes_modelos/")
+    def __str__(self):
+        return self.color
 
+# Tabla de los vehiculos
 class Vehiculos(models.Model):
+    class Meta:  
+            verbose_name_plural = 'Vehiculos'
+
+    def validar_cuatro_digitos(value):
+        if not (1800 <= value <= 3000):
+            raise ValidationError("El valor ingresado debe ser un año.")
+        
     AUTO = "Auto"
     PICKUP = "Pickup"
     COMERCIAL = "Comercial"
@@ -28,13 +43,49 @@ class Vehiculos(models.Model):
     id_vehiculos = models.AutoField(primary_key=True)
     vehiculo = models.CharField(max_length=100, blank=False, null=False, unique=True)
     precio = models.FloatField(null=True, blank=False)
-    fecha = models.DateField(null=True, blank=False, default=timezone.now)
+    fecha = models.IntegerField(validators=[validar_cuatro_digitos], null=True, blank=False)
     tipo = models.CharField(max_length=10, choices=OPCIONES)
-    imagenes = models.ManyToManyField(Imagenes)
+
+    def __str__(self):
+        return self.vehiculo
+
+    # Formatear el precio con separadores de miles
+    @property
+    @admin.display(description='precio', ordering='precio')
+    def price_formatted(self):
+        return f"{self.precio:,}"
+
     
 
+# Tabla modelos de los vehiculos, cada vehiculo puede tener mas de un modelo
 class ModelosVehiculos(models.Model):
+    class Meta:  
+        verbose_name_plural = 'Modelos de Vehiculos'
+
     id_modelos_vehiculos = models.AutoField(primary_key=True)
-    id_vehiculo = models.ForeignKey(Vehiculos, on_delete=models.CASCADE)
+    vehiculo = models.ForeignKey(Vehiculos, on_delete=models.CASCADE)
     modelo = models.CharField(max_length=100, null=False, blank=False)
     colores = models.ManyToManyField(Colores)
+
+    def __str__(self):
+        return self.modelo
+
+# Imagenes relacionadas al vehiculo
+class Imagenes(models.Model):
+    class Meta:  
+        verbose_name_plural = 'Imagenes'
+
+    id_imagen_modelo = models.AutoField(primary_key=True)
+    imagen = models.ImageField(null=False, blank=False, upload_to="imagenes_modelos/")
+    vehiculo = models.ForeignKey(Vehiculos, on_delete=models.CASCADE)
+
+    def __str__(self):
+            return self.imagen.path
+
+# Funcion que elimina la imagen en la carpeta "media/imagenes_modelos/" cuando se borra la instacia asociada
+#   a la misma en la tabla Imagenes
+@receiver(pre_delete, sender=Imagenes)
+def borrar_imagen(sender, instance, **kwargs):
+    if instance.imagen:
+        if os.path.isfile(instance.imagen.path):
+            os.remove(instance.imagen.path)
